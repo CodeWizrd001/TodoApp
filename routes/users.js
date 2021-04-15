8fa30e10b0a47e385f91eabbda2e46585de0d2d5
Wizard <codewizard001@gmail.com>
2021-04-15 17:10:56 -0400

    Completed User Funcs 		Added User Signup 		Added User Login 		Added User View Tasks with Bootstrap Cards

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,64 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var {db , User , Task} = require('../models') ;
+
+// GET Requests 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users');
 });
+
+router.get('/login',function(req,res,next){
+  res.render('login',{failed:false})
+}) ;
+
+router.get('/signup',function(req,res,next){
+  res.render('signup',{failed:false}) ;
+}) ;
+
+router.get('/:user',async function(req,res,next){
+  var username = req.params.user ;
+  var taskList = await Task.find({user:username}) ;
+  res.render('dashboard',{user:username,list:taskList}) ;
+}) ;
+
+// POST Requests 
+router.post('/login',async function(req,res,next){
+  var username = req.body.username ;
+  var password = req.body.password ;
+  var user = await User.findOne({
+    name : username ,
+    password : password 
+  })
+  if(!user)
+    res.render('login',{failed:true}) ;
+  else 
+    res.redirect(username) ;
+}) ;
+
+router.post('/signup',async function(req,res,next){
+  var username = req.body.username ;
+  var password = req.body.password ;
+  var user = await User.findOne({
+    name : username ,
+    password : password 
+  })
+  if(user)
+    res.render('signup',{failed:true}) ;
+  else 
+  {
+    var tempUser = User({
+      name : username,
+      password : password ,
+    })
+    tempUser.save()
+    res.redirect('login') ;
+  }
+}) ;
+
+router.post('/:user/add',async function(req,res,next){
+  var username = req.params.user;
+  res.redirect(username) ; 
+}) ;
 
 module.exports = router;
